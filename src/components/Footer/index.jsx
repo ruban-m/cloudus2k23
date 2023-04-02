24f2e27e63b85d035e210f7b07dcd69a5a03291c
ruban-m <rubantamilboy@gmail.com>
2023-04-02 17:09:07 +0530

    vanmam

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,14 +4,15 @@ import './style.css'
 import bgImage from '/assets/images/background.png'
 
 const Footer = () => {
-  const [footerHeight, setfooterHeight] = useState(0)
-  const sendHeight = (message) => {
-    setfooterHeight(message)
-  }
+  // const [footerHeight, setfooterHeight] = useState(0)
+  // const sendHeight = (message) => {
+  //   setfooterHeight(message)
+  // }
 
   return (
-    <div className='footer' style={{ minHeight: `${footerHeight + 10}px` }}>
-      <Contact callBack={sendHeight} />
+    <div className='footer' style={{ minHeight: '300px' }}>
+      {/* style={{ minHeight: `${footerHeight + 10}px` }} */}
+      {/* <Contact callBack={sendHeight} /> */}
       <img src={bgImage} alt='' className='nightimage' />
     </div>
   )
